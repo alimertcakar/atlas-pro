15e79f870a21695aff588971d1f212aecb22ac19
Ali Mert Çakar <mail@alimert.net>
2024-04-11 19:15:59 +0300

    code

--- src/pages/content/injected/index.ts
+++ src/pages/content/injected/index.ts
@@ -12,3 +12,44 @@ import('@root/src/shared/storages/mainStorage');
 if (window.location.host === 'bitbucket.org') {
   document.body.classList.add('atlassian_pro_enabled');
 }
+
+// text nodes needs to be wrapped in span elements in order to be styled
+
+function wrapTextNodes(spanElement) {
+  var childNodes = spanElement.childNodes;
+
+  childNodes.forEach(function (node) {
+    if (node.nodeType === Node.TEXT_NODE) {
+      var span = document.createElement('span');
+      span.textContent = node.textContent;
+      node.parentNode.replaceChild(span, node);
+    } else if (node.nodeType === Node.ELEMENT_NODE) {
+      wrapTextNodes(node);
+    }
+  });
+}
+
+const wrapAllTextNodesInsideCode = () => {
+  document.querySelectorAll('.code-component .css-0').forEach(function (node) {
+    wrapTextNodes(node);
+  });
+};
+
+const config = { childList: true, subtree: true };
+
+const callback = function (mutationsList) {
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      mutation.addedNodes.forEach(node => {
+        if (node.nodeType === 1 && node.classList.contains('.code-component')) {
+          wrapAllTextNodesInsideCode();
+        }
+      });
+    }
+  }
+};
+
+const observer = new MutationObserver(callback);
+
+const targetNode = document.querySelector('body');
+observer.observe(targetNode, config);
